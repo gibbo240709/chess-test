9912d1c179efdc72605af2ffcf673e1e11fb6b9d
gibbo240709 <scottgibbons07@gmail.com>
2026-02-10 13:46:52 +0000

    fonts
    added fonts

--- board.py
+++ board.py
@@ -12,4 +12,6 @@ HEIGHT = 900
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Chess")
-
+font = pygame.font.Font("freesansbold.ttf", 20)
+medium_font = pygame.font.Font("freesansbold.ttf", 40)
+big_font = pygame.font.Font("freesansbold.ttf", 50)
