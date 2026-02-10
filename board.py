8ba83efe5762ede43ab7b6114a8f317585e21a48
gibbo240709 <scottgibbons07@gmail.com>
2026-02-10 13:48:00 +0000

    timer

--- board.py
+++ board.py
@@ -15,3 +15,6 @@ pygame.display.set_caption("Chess")
 font = pygame.font.Font("freesansbold.ttf", 20)
 medium_font = pygame.font.Font("freesansbold.ttf", 40)
 big_font = pygame.font.Font("freesansbold.ttf", 50)
+
+timer = pygame.time.Clock()
+fps = 60
