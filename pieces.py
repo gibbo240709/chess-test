64423d07633ff76266d09b77c4d3656d5c4777f3
gibbo240709 <scottgibbons07@gmail.com>
2026-02-10 14:00:58 +0000

    new pieces file and code for white pieces

---
+++ pieces.py
@@ -0,0 +1,4 @@
+white_pieces = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook",
+                "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
