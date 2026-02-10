4b9c16365fde51a5737a7f7f0aa0b2b83a3240b0
gibbo240709 <scottgibbons07@gmail.com>
2026-02-10 13:43:01 +0000

    basic display
    very basic display

--- board.py
+++ board.py
@@ -3,4 +3,13 @@ import requests
 import rembg
 from io import BytesIO
 
+#initialise pygame
+pygame.init()
+
+#width and height of screen
+WIDTH = 1000
+HEIGHT = 900
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("Chess")
 
